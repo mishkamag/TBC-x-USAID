43666fc57414263f7381045126dd5f0596fbd08c
mishkamag <m93.maglaperidze@gmail.com>
2024-01-19 23:47:43 +0400

    feat: add faq section with show/hide questions feature

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const header = document.getElementById("mainHeader");
 const menuBtn = document.getElementById("menuBtn");
 const mainNav = document.getElementById("mainNav");
 const container = document.getElementById("card-container");
+const buttons = document.querySelectorAll("button");
 
-//add transparant for header
+//add transparant for header when scroll down
+
 document.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
     header.classList.add("transparent");
@@ -13,6 +15,8 @@ document.addEventListener("scroll", function () {
     header.classList.remove("transparent");
   }
 });
+
+//burger icon show and hide
 
 menuBtn.addEventListener("click", function () {
   mainNav.classList.toggle("mobile-links");
@@ -29,10 +33,11 @@ document.addEventListener("click", function (event) {
   }
 });
 
+// All course's card render
+
 cardData.map((card) => {
   const cardElement = document.createElement("div");
   cardElement.className = "card";
-
   cardElement.innerHTML = `
       <img src="${card.imgUrl}" alt=""/>
       <div class="card__info">
@@ -41,6 +46,30 @@ cardData.map((card) => {
       </div>
       <span class="card__about">${card.about}</span>
     `;
-
   container.appendChild(cardElement);
+});
+
+// FAQ for show/hide question, also rotate icon
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentFaq = button.nextElementSibling;
+    const currentIcon = button.querySelector(".d-arrow");
+
+    if (currentFaq && currentIcon) {
+      buttons.forEach((otherButton) => {
+        if (otherButton !== button) {
+          const otherFaq = otherButton.nextElementSibling;
+          const otherIcon = otherButton.querySelector(".d-arrow");
+
+          if (otherFaq && otherIcon) {
+            otherFaq.classList.remove("show");
+            otherIcon.classList.remove("rotate");
+          }
+        }
+      });
+      currentFaq.classList.toggle("show");
+      currentIcon.classList.toggle("rotate");
+    }
+  });
 });
